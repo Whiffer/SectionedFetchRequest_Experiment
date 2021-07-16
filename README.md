51cfef3a9901cddb3d634defbe9bc1d62ca1cf4d
Whiffer <Chuck@ForeTheGreen.com>
2021-07-16 15:15:01 -0700

    Updated ContentView with new workaround for the issue. Updated Comments and README.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SectionedFetchRequest_Experiment
 
-Updated: July 11, 2021
+Updated: July 16, 2021
 
-I am in the process of updating my SwiftUI-Core-Data-Test project to use the new Dynamic Properties that were introduced in the WWDC21-10017 session.  I'm pretty much done, except that I'm having one issue with the SectionedFetchRequest. This is a sample project that demonstrates the issue that I'm having.  My instinct is that the issue is a minor oversight with the iOS 15 Beta code, however, I am open to alternative interpretations.
+I am in the process of creating a companion project to my SwiftUI-Core-Data-Test project that will use the new Property Warappers and Dynamic Properties that were introduced in the WWDC21-10017 session.  I was pretty much done, except that I was having one issue with the new @SectionedFetchRequest Property Wrapper. I have now developed what I think is a suitable workaround for this issue and will now resume working to complete the new sample project.
 
-My issue is documented in the comments in the ContentView, which includes a hack as a possible workaround.  However, I would prefer a more straight forward solution or answer.
+My issue, and my workaround for it, are documented in the comments in the ContentView of this project. Please let me know if there are any questions or concerns with this experiment with @SectionedFetchRequest.
+
+My instinct is that this issue is an oversight with the implementation of the @SectionedFetchRequest Property Wrapper in the iOS 15 Beta code, however, I am open to alternative interpretations.  I will be submitting a Feedback request to Apple as well.
--- SectionedFetchRequest_Experiment/ContentView.swift
+++ SectionedFetchRequest_Experiment/ContentView.swift
@@ -14,27 +14,28 @@ struct ContentView: View {
 
     // The @SectionedFetchRequest is configured as follows:
     
-    // By using the \.sectionName keypath for the sectionIdentifier, all of the Attribute objects
+    // By using the \Attribute.sectionName keypath for the sectionIdentifier, all of the Attribute objects
     // that are related to the same Item object will be grouped into the same List Section.
     
     // The first SortDescriptor sorts all of the Attribute objects based on the value of
-    // the \.item.order keypath.  i.e. The order of the Sections is a function of a value property
-    // of the related Item and not a function of any value property of the Attribute.
+    // the \Attribute.item.order keypath.  i.e. The order of the Sections is a function of a value property
+    // of the related Item object and not a function of any value property of the Attribute.
 
     // In WWDC21-10017 (@ 24:40), Scott Perry warns that the sectionIdentifier and the first SortDescriptor
     // must be coordinated for the Sections to be grouped properly.
     // In this case, since the sectionIdentifier and the sortDescriptors are never dynamically updqated and
-    // the values of both \.sectionName and \.item.order are always based on
+    // the values of both \Attribute.sectionName and \Attribute.item.order are always based on
     // the same related Item object, they are guaranteeded to be (and stay) in sync.
     
-    @SectionedFetchRequest(sectionIdentifier: \.sectionName,
-                           sortDescriptors: [SortDescriptor(\.item.order, order: .forward),
-                                             SortDescriptor(\.order, order: .forward)],
+    @SectionedFetchRequest(sectionIdentifier: \Attribute.sectionName,
+                           sortDescriptors: [SortDescriptor(\Attribute.item.order, order: .forward),
+                                             SortDescriptor(\Attribute.order, order: .forward)],
                            predicate: nil,
                            animation: .default)
     private var attributes: SectionedFetchResults<String, Attribute>
     
-    @State private var state = false
+    // WORKAROUND: A Publisher that generates an event when the Managed Object Context gets saved
+    private var contextDidSave =  NotificationCenter.default.publisher(for: .NSManagedObjectContextDidSave)
 
     var body: some View {
         NavigationView {
@@ -52,6 +53,75 @@ struct ContentView: View {
             .navigationBarItems(leading: Button("Reset", action: { async { await self.reset() } } ),
                                 trailing: Button("Swap", action: { async { await self.swap() } } ) )
         }
+        
+        // BEGIN WORKAROUND
+        // Uncommenting the following will resolve the issue of the View not being updated when the "Swap" button is tapped.
+//        .onReceive(self.contextDidSave) { notification in
+//            self.handleContextDidSaveNotification(notification: notification)
+//        }
+        // END WORKAROUND
+    }
+    
+    private func handleContextDidSaveNotification(notification: Notification) {
+        
+        // Don't bother if there are no results
+        guard attributes.count > 0 else { return }
+        
+        // A more general solution should probably use the SortDescriptor key paths
+        // to help decide whether or not an updated object will affect the results sort order.
+//        for sortDescriptor in attributes.sortDescriptors {
+//            let sortDescriptor = NSSortDescriptor(sortDescriptor)
+//            print("SortDescriptor Keypath: \(sortDescriptor.key ?? "")")
+//        }
+        
+        // This workaround only checks for "updated" objects
+        // A more general solution may need to check other notification types as well
+        
+        // Get the Set of NSManagedOject's that were updated when the Context was saved.
+        if let updates = notification.userInfo?["updated"] as? Set<NSManagedObject> {
+
+            // For performance reasons, the results should be refreshed only when it is really necessary.
+            // The code below attempts to do that by comparing each updated object to all of the objects
+            // that have a relationship with each Results object.
+            // Additional/Different tests may be needed for a completely general solution.
+            
+            var refreshResultsNeeded = false
+            
+            outerLoop: for update in updates {
+                if let updatedEntityName = update.entity.name {
+                    // Since there is results, there should always be at least one Section
+                    let section = attributes[0]
+                    // That Section should always contain at least one NSManagedObject
+                    for relationship in section[0].entity.relationshipsByName.values {
+                        // Get Name of an Entity related to the Results object
+                        if let destinationEntityName = relationship.destinationEntity?.name {
+                            let destinationKeyPath = relationship.name
+                            // Is the Entity Name of the updated object the same as the Entity that is related to Results?
+                            if updatedEntityName == destinationEntityName {
+                                // If so, is the updated object the same object that is related to a Results object?
+                                for section in attributes {
+                                    for attribute in section {
+                                        let relatedObject = attribute.value(forKey: destinationKeyPath) as! NSManagedObject
+                                        if relatedObject.objectID == update.objectID {
+                                            // The results should be refetched since an object that is related to a Results
+                                            // object has been updated and could have changed the order of the sections.
+                                            refreshResultsNeeded = true
+                                            break outerLoop
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if refreshResultsNeeded {
+                // This is a trick that causes the SectionedFetchResults to be refetched by toggling the nsPredicate
+                // dynamic property between two different values that always select all Attribute objects.
+                attributes.nsPredicate = (attributes.nsPredicate == nil ? NSPredicate(format: "1==1") : nil)
+            }
+        }
     }
     
     @MainActor
@@ -63,29 +133,27 @@ struct ContentView: View {
     @MainActor
     func swap() async {
         
-        let context = PersistenceController.shared.container.viewContext
-
         do {
             // The following code swaps the order properties of the first two Item objects.
             
-            let items = try context.fetch(Item.fetchAllInOrder())
+            let items = try self.viewContext.fetch(Item.fetchAllInOrder())
             let firstItemOrder = items[0].order
             let secondItemOrder = items[1].order
             items[0].order = secondItemOrder
             items[1].order = firstItemOrder
-            try context.save()
+            try self.viewContext.save()
             
             // Because none of the Item "names" have changed, all of the Attribute objects will
             // still be grouped together into the same Section with the same name.
             
-            // However, because the "order" of the Item objects has been changed,
+            // However, because the "order" of the "Item" objects has been changed,
             // the order of the Attribute objects in the fetched results (and therefore
             // the order of Sections) should be different as well.
             // These committed changes should cause the fetched results to be refreshed and
             // in turn cause SwiftUI to update the View.
             
             // In this case, the View is not updated unless the App is terminated and restarted or
-            // unless the workaround trick from below is enabled.
+            // unless the workaround from above is enabled.
             
             // BTW, changes committed to any value property of an Attribute object will
             // cause the fetched results (and the View) to be updated properly.
@@ -93,29 +161,6 @@ struct ContentView: View {
         } catch let error as NSError {
             fatalError("Unresolved error \(error), \(error.userInfo)")
         }
-        
-        //**********************************************************
-        // WORKAROUND Trick
-        //
-        // Uncommenting this code will get SwiftUI to update the fetched results for
-        // attributes:SectionedFetchResults<String, Attribute> by toggling a @State var that triggers
-        // a change to the nsPredicate dynamic property.
-        // This works even though the changed nsPredicate property value has no real effect on the results fetched.
-        
-//        self.state.toggle()
-//        self.attributes.nsPredicate = (self.state ? NSPredicate(format: "1==1") : NSPredicate(format: "0==0"))
-        //**********************************************************
-        
-        //**********************************************************
-        // QUESTION - Can this be made to work?
-        //
-        // The documentation for the update() method seems to imply that new results should be fetched
-        // when it is called,  However, when it is called, the fetched results do not seem to change and the
-        // View does not get updated.
-        
-//        var attributes = self.$attributes.projectedValue
-//        attributes.update()
-        //**********************************************************
     }
     
 }
